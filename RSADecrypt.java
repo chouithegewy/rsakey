6dfcf0eb12763dc639f4df6c60c388e0aeba18ca
David Nunes-Childs <combatburrito@gmail.com>
2026-05-01 17:46:25 -0700

    encrypt ok, decrypt ok, keygen ok

--- RSADecrypt.java
+++ RSADecrypt.java
@@ -1,3 +1,4 @@
+
 package rsa;
 
 import java.io.BufferedReader;
@@ -11,11 +12,11 @@ import java.nio.CharBuffer;
 import java.text.Format;
 import java.util.HashMap;
 
-class RSAEncrypt {
+class RSADecrypt {
     static private String encoding = "abcdefghijklmnopqrstuvwxyz ";
-    static private String pubkeyPath;
+    static private String prikeyPath;
     static private String inputPath;
-    static private HashMap<String, BigInteger> pubkey = new HashMap<>();
+    static private HashMap<String, BigInteger> prikey = new HashMap<>();
 
     static void main(String[] args) {
         try {
@@ -24,30 +25,30 @@ class RSAEncrypt {
             e.printStackTrace();
             System.exit(-1);
         }
-        pubkey = getPubKey(pubkeyPath);
+        prikey = getPriKey(prikeyPath);
         // t, h, i
-        BigInteger plaintext = BigInteger.valueOf(190708);
+        // BigInteger plaintext = BigInteger.valueOf(190708);
         // e =
         // n =
         // BigInteger e = BigInteger.valueOf(8311);
-        BigInteger e = pubkey.get("e");
+        BigInteger d = prikey.get("d");
         // BigInteger n = BigInteger.valueOf(31005883);
-        BigInteger n = pubkey.get("n");
-        BigInteger c = plaintext.modPow(e, n);
-        BigInteger d = BigInteger.valueOf(11296191);
-        BigInteger p = c.modPow(d, n);
-        encryptFile(inputPath);
+        BigInteger n = prikey.get("n");
+        // BigInteger c = plaintext.modPow(d, n);
+        // BigInteger d = BigInteger.valueOf(11296191);
+        // BigInteger p = c.modPow(d, n);
+        decryptFile(inputPath);
     }
 
-    static HashMap<String, BigInteger> getPubKey(String path) {
+    static HashMap<String, BigInteger> getPriKey(String path) {
         HashMap<String, BigInteger> en = new HashMap<>();
         try (BufferedReader bf = new BufferedReader(new FileReader(path))) {
             String s;
 
             while ((s = bf.readLine()) != null) {
                 String[] split = s.split(" = ");
-                if (en.get("e") == null) {
-                    en.put("e", BigInteger.valueOf(Long.valueOf(split[1])));
+                if (en.get("d") == null) {
+                    en.put("d", BigInteger.valueOf(Long.valueOf(split[1])));
                 } else {
                     en.put("n", BigInteger.valueOf(Long.valueOf(split[1])));
                     if (Long.valueOf(split[1]) <= 262626) {
@@ -64,29 +65,28 @@ class RSAEncrypt {
         return en;
     }
 
-    static String encryptFile(String path) {
-        String encrypted = "";
+    static String decryptFile(String path) {
+        String decrypted = "";
 
         try (BufferedReader bf = new BufferedReader(new FileReader(path))) {
             String s;
-            try (BufferedWriter out = new BufferedWriter(new FileWriter("test.enc"))) {
+            try (BufferedWriter out = new BufferedWriter(new FileWriter("test.dec"))) {
                 while ((s = bf.readLine()) != null) {
+                    s = s.replace(" ", ""); // join string, keep previous logic
                     String encodingValString = "";
-                    for (int i = 0, j = 1; i < s.length(); ++i, ++j) {
-                        char c = s.charAt(i);
-                        int encodingVal = encoding.indexOf(c);
-                        encodingValString += String.format("%02d", encodingVal);
-                        if (j % 3 == 0) {
-                            // do work on encoded integer then reset
-                            BigInteger encodingAsBigInt = BigInteger.valueOf(Long.parseLong(encodingValString));
-                            BigInteger ciphertext = encodingAsBigInt.modPow(pubkey.get("e"), pubkey.get("n"));
-
-                            out.write(ciphertext.toString() + " ");
-
-                            encodingValString = "";
-                        } else {
-                            // keep building string
+                    for (int i = 0, j = 6; i < s.length(); i = i + 6, j = j + 6) {
+                        BigInteger cipherBlock = BigInteger.valueOf(Long.valueOf(s.substring(i, j)));
+                        // retrieve plaintext then decode
+                        String plaintext = cipherBlock.modPow(prikey.get("d"), prikey.get("n")).toString();
+                        String encodedPlaintext = "";
+                        for (int k = 0; k < plaintext.length() - 1; k = k + 2) {
+                            int index = Integer.parseInt(plaintext.substring(k, k + 2));
+                            char encodedVal = encoding.charAt(index);
+                            encodedPlaintext += "" + encodedVal;
                         }
+                        out.write(encodedPlaintext);
+
+                        encodedPlaintext = "";
                     }
                 }
             } catch (IOException ioe) {
@@ -99,15 +99,15 @@ class RSAEncrypt {
             System.exit(1);
         }
 
-        return encrypted;
+        return decrypted;
     }
 
     static void parseArgs(String[] args) throws Exception {
         if (args.length != 2) {
-            throw new Exception("Incorrect number of arguments. Expected 2. <input> <pubkey>");
+            throw new Exception("Incorrect number of arguments. Expected 2. <input> <prikey>");
         } else {
             inputPath = args[0];
-            pubkeyPath = args[1];
+            prikeyPath = args[1];
         }
     }
 }
